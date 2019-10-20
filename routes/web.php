4f44a004dc5fcc22a1c9522104b862ac040c2939
jebauza <jebauza@gmail.com>
2019-10-20 11:38:28 +0200

    Para el project

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Str;
+
 
 Route::get('/', function () {
     //return view('welcome');
@@ -18,22 +20,16 @@ Route::get('/', function () {
 
 Route::resource('feed', 'FeedController');
 
-/*Route::get('/prueba', function() {
+Route::get('/prueba', function() {
 
-    $crawler = Goutte::request('GET', 'https://www.elpais.com/');
-    $crawler->filter('#bloque_actualidad_destacadas .bloque__interior article.articulo')->each(function ($node,$i=0) {
-      
-      if($i<5)
-      {
-        $arr = [] ;
-        $link = $node->filter('h2.articulo-titulo a');
-        $arr['title'] = $link->text();
-        $url = $link->attr('href');
-        $arr['autor'] = $node->filter('span.autor-nombre a')->text();
-        $arr['image'] = $node->filter('figure.foto img')->attr('src');
 
-        dump($arr);
-      }
-      $i++;
-    });
+  //antes de esto setear el archivo de configuracion
+  $directory = 'image/';
+  $files = Storage::disk('public')->files($directory);
+  $filesUrl = [];
+  foreach($files as $file)
+  {
+    $filesUrl[] = ['nameFile'=>Str::replaceFirst($directory, '', $file),'urlFile'=>Storage::disk('public')->url($file)];
+  }
+  dd($filesUrl);
 });
